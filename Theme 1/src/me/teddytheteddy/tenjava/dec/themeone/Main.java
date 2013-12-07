f5f5ecb262ff90c3a0f3d0c5e63ca0a323061349
TeddyTheTeddy <teddy-the-teddy@live.com>
2013-12-07 08:14:27 +0000

    Added basic main class/ plugin.yml structure and added config base.

--- Theme 1/src/me/teddytheteddy/tenjava/dec/themeone/Main.java
+++ Theme 1/src/me/teddytheteddy/tenjava/dec/themeone/Main.java
@@ -1,10 +1,35 @@
 package me.teddytheteddy.tenjava.dec.themeone;
 
+import java.io.File;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  *
  * @author TeddyTheTeddy
  * 
  */
-public class Main {
+public class Main extends JavaPlugin{
+    //Variables
+    public File configFile;
+    public FileConfiguration config = null;
     
+    //onEnable
+    @Override
+    public void onEnable(){
+        
+        //Configuration File
+        configFile = new File(this.getDataFolder(), "config.yml");
+        configFile.mkdir();
+        
+        config = this.getConfig();
+        this.saveConfig();
+        
+    }
+    
+    //onDisable
+    @Override
+    public void onDisable(){
+        
+    }
 }
